44e77fa5b57a734eb3aabec746ccc01717778671
taep96 <64481039+taep96@users.noreply.github.com>
2022-05-05 19:51:21 +0200

    patch: cool-autostart

--- config.h
+++ config.h
@@ -18,6 +18,12 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_fg3,   col_bg2,   col_bg2   },
 };
 
+// #define SHCMD(cmd) "/bin/sh", "-c", cmd, NULL
+static const char *const autostart[] = {
+    "sh", "~/.autostart.sh", NULL,
+    NULL
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
