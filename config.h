315237b74526a6aaeeaf4c1f31d68460e068ed08
taep96 <64481039+taep96@users.noreply.github.com>
2022-05-07 20:17:04 +0200

    style: more spaces

--- config.h
+++ config.h
@@ -120,15 +120,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,        focusmon,         { .i  = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,         tagmon,           { .i  = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,        tagmon,           { .i  = +1 } },
-	TAGKEYS(                        XK_1,                               0 )
-	TAGKEYS(                        XK_2,                               1 )
-	TAGKEYS(                        XK_3,                               2 )
-	TAGKEYS(                        XK_4,                               3 )
-	TAGKEYS(                        XK_5,                               4 )
-	TAGKEYS(                        XK_6,                               5 )
-	TAGKEYS(                        XK_7,                               6 )
-	TAGKEYS(                        XK_8,                               7 )
-	TAGKEYS(                        XK_9,                               8 )
+	TAGKEYS(                        XK_1,                                 0 )
+	TAGKEYS(                        XK_2,                                 1 )
+	TAGKEYS(                        XK_3,                                 2 )
+	TAGKEYS(                        XK_4,                                 3 )
+	TAGKEYS(                        XK_5,                                 4 )
+	TAGKEYS(                        XK_6,                                 5 )
+	TAGKEYS(                        XK_7,                                 6 )
+	TAGKEYS(                        XK_8,                                 7 )
+	TAGKEYS(                        XK_9,                                 8 )
 	{ MODKEY|ShiftMask,             XK_q,             quit,             { 0 } },
 	{ MODKEY|ShiftMask,             XK_r,             quit,             { 1 } }, 
 };
